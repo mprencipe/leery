bdab217be71b9148e3b0993c3b8e3825e76006fd
Massimo Prencipe <mprencipe@gmail.com>
2020-03-09 21:07:22 +0200

    Refactor, modularize, comment

--- background.js
+++ background.js
@@ -1,41 +1,4 @@
-browser.storage.local.get().then(store => {
-    if (!store.data) {
-        store = {
-            data: {}
-        };
-    }
-
-    function responseListener(requestDetails) {
-        const origin = requestDetails.originUrl;
-
-        if (store.data[origin] == null) {
-            store.data[origin] = {};
-        }
-
-        if (requestDetails.type != 'xmlhttprequest') {
-            return;
-        }
-
-        const corsHeader = requestDetails.responseHeaders.find(h => h.name.toLowerCase() == 'access-control-allow-origin')
-        if (corsHeader != null && corsHeader.value == '*') {
-            store.data[origin]['cors-star'] = true;
-            browser.storage.local.set(store);
-        }
-
-        browser.tabs.query({ currentWindow: true, active: true })
-            .then((tabs) => {
-                if (tabs[0].url == origin) {
-                    setText(tabs[0].url);
-                }
-            });
-    }
-
-    browser.webRequest.onHeadersReceived.addListener(
-        responseListener,
-        { urls: ["<all_urls>"] },
-        ["responseHeaders"]);
-});
-
+// set notification when switching to a new tab
 browser.tabs.onActivated.addListener((activeInfo) => {
     browser.tabs.query({ currentWindow: true, active: true })
         .then((tabs) => {
@@ -43,6 +6,7 @@ browser.tabs.onActivated.addListener((activeInfo) => {
         });
 });
 
+// set notification text if site has abnormalities
 function setText(url) {
     browser.storage.local.get().then(store => {
         const data = store.data != null && store.data[url] != null ? store.data[url] : {};
@@ -52,3 +16,60 @@ function setText(url) {
         });
     });
 }
+
+function onHeadersReceived(requestDetails) {
+
+    browser.storage.local.get().then(store => {
+
+        // ensure that data object always exists so other code doesn't blow up
+        if (!store.data) {
+            store = {
+                data: {}
+            };
+        }
+
+        // if site has no existing data, add an empty data object
+        if (store.data[requestDetails.originUrl] == null) {
+            store.data[requestDetails.originUrl] = {};
+        }
+
+        handleHeaders(requestDetails, store);
+
+        browser.tabs.query({ currentWindow: true, active: true })
+            .then((tabs) => {
+                if (tabs[0].url == requestDetails.originUrl) {
+                    setText(tabs[0].url);
+                }
+            });
+
+    });
+
+}
+
+function ajaxRequest(requestDetails) {
+    return requestDetails.type === 'xmlhttprequest';
+}
+
+function handleCors(requestDetails, store) {
+    if (!ajaxRequest(requestDetails)) {
+        return;
+    }
+
+    const corsHeader = requestDetails.responseHeaders.find(h => h.name.toLowerCase() == 'access-control-allow-origin')
+    if (corsHeader != null && corsHeader.value == '*') {
+        store.data[requestDetails.originUrl]['cors-star'] = true;
+        browser.storage.local.set(store);
+    }
+}
+
+// set site data based on header abnormalities
+function handleHeaders(requestDetails, store) {
+
+    handleCors(requestDetails, store);
+
+}
+
+browser.webRequest.onHeadersReceived.addListener(
+    onHeadersReceived,
+    { urls: ["<all_urls>"] },
+    ["responseHeaders"]);
